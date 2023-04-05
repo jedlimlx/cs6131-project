7f12db1ac831a9cce1923d117592fe4fe0c6a493
jedlimlx <jedlimlx@outlook.com>
2023-04-05 18:05:57 +0800

    Login, logout magic working

--- client/src/main.ts
+++ client/src/main.ts
@@ -8,7 +8,7 @@
 import App from './App.vue'
 
 // Composables
-import { createApp } from 'vue'
+import { createApp, watch } from 'vue'
 
 // Plugins
 import { registerPlugins } from '@/plugins'
@@ -24,7 +24,6 @@ import { createPinia } from "pinia"
 import piniaPluginPersistedState from "pinia-plugin-persistedstate"
 
 const pinia = createPinia()
-
 pinia.use(piniaPluginPersistedState)
 
 export const SERVER = "http://localhost:5000"
--- client/src/store/app.ts
+++ client/src/store/app.ts
@@ -1,16 +1,23 @@
 // Utilities
-import { createPinia, defineStore } from 'pinia'
-import { ref } from "vue";
-import { SERVER } from "@/main";
+import { defineStore } from 'pinia'
+import { SERVER } from "@/main"
+import { useLocalStorage } from "@vueuse/core"
 
 export const useUserStore = defineStore('user', {
     state: () => ({
-        loggedIn: false,
-        username: "",
-        firstname: "",
-        lastname: "",
-        uid: -1,
-        email: "",
+        //@ts-ignore
+        loggedIn: useLocalStorage("loggedIn", false),
+        //@ts-ignore
+        username: useLocalStorage("username", ""),
+        //@ts-ignore
+        firstname: useLocalStorage("firstname", ""),
+        //@ts-ignore
+        lastname: useLocalStorage("lastname", ""),
+        //@ts-ignore
+        uid: useLocalStorage("uid", -1),
+        //@ts-ignore
+        email: useLocalStorage("email", ""),
+        //@ts-ignore
         creatingNewAccount: false
     }),
     actions: {
@@ -25,11 +32,26 @@ export const useUserStore = defineStore('user', {
                 this.lastname = data.lastname
                 this.email = data.email
                 this.uid = data.uid
-                console.log(data)
             }
 
             return this.loggedIn
         },
-    },
-    persist: true
+        logout() {
+            // Edit local storage
+            localStorage.removeItem("loggedIn")
+            localStorage.removeItem("username")
+            localStorage.removeItem("firstname")
+            localStorage.removeItem("lastname")
+            localStorage.removeItem("uid")
+            localStorage.removeItem("email")
+
+            // Reset to original variables
+            this.loggedIn = false
+            this.username = ""
+            this.firstname = ""
+            this.lastname = ""
+            this.uid = -1
+            this.email = ""
+        }
+    }
 })
